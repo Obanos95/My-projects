7f43c5314315bbdd8613c0ebf42c9d9d115c7350
Obanos95 <sgaribov451@gmail.com>
2024-11-26 12:54:34 +0400

    Added libraries for PW6

---
+++ Programming/Python/PWs/PW6/functions_text.py
@@ -0,0 +1,44 @@
+
+def number_differents_characters(text):
+    '''
+    number of differents characters in a given text.
+
+    :param text: text to analyse
+    :type text: str
+    :return: number of differents characters
+    :type: int
+    '''
+    assert isinstance(text, str)
+    pass
+
+
+def number_words(text):
+    '''
+    number of words in a text.
+    
+    It is assumed that these are the spaces that allow words to be recognized. 
+    The words are between spaces.
+
+    :param text: text to analyse
+    :type text: (str)
+    :return: number of words in a text
+    :type: (int)
+    '''
+    assert isinstance(text, str)
+    pass
+
+
+def number_words_differents(text):
+    '''
+    number of differents words in a text.
+    
+    It is assumed that these are the spaces that allow words to be recognized. 
+    The words are between spaces.
+
+    :param text: text to analyse
+    :type text: str
+    :return: number of differents words in a text
+    :type: (int)
+    '''
+    assert isinstance(text, str)
+    pass
