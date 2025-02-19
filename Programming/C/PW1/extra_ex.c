25b9e2bd3d786419bdd7b89d01f774eac42216c7
Obanos95 <sgaribov451@gmail.com>
2025-02-19 13:46:37 +0400

    Changed extra_ex because of some additional info

--- Programming/C/PW1/extra_ex.c
+++ Programming/C/PW1/extra_ex.c
@@ -6,7 +6,7 @@ bool ifPrime(int number){
 	int numOfDivisors = 0;
 	bool isPrime = true;
 
-	for (int i = 1;i < (int)sqrt(number);i++){
+	for (int i = 1;i < (int)sqrt(number) + 1;i++){
 		if (number % i == 0){
       			numOfDivisors++;
       		}
@@ -24,7 +24,6 @@ bool ifPrime(int number){
 int main(){
 
 	int numb;
-	bool found = false;
 	int num1,num2;
 
       	puts("Enter number you want to show as N+N");
@@ -32,18 +31,10 @@ int main(){
 
       	for (int i = 1;i < numb/2;i++){
 		if (ifPrime(i) == true && ifPrime(numb-i) == true){
-			found = true;
 			num1 = i;
 			num2 = numb-i;
-			break;
+			printf("%d + %d\n",num1,num2);
 		}
 	}
-	if (found == true){
-
-		printf("%d is %d + %d\n",numb,num1,num2);
-
-	}
-	else
-		puts("There is no way of showing the number as N+N");
 	return 0;
 }
